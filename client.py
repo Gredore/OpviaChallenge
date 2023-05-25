c42163a16f8a23b59c59285cfd034fdcaf4a5a09
George Irving <george.irving1@gmail.com>
2023-05-25 21:25:11 +0100

    Tidying up client.py

--- client.py
+++ client.py
@@ -32,15 +32,32 @@ class SFTPClientRunner:
 
     @staticmethod
     def get_controls(path):
+        """
+        Reads control file json
+        :param path: path to find the json control file
+        :return: diction of control file data
+        """
         with open(path, "r") as f:
             return json.load(f)
 
     @staticmethod
     def get_key(key_path, password):
+        """
+        Makes RSAKey from key file path
+        :param key_path: path to the private key
+        :param password: password encrypting the key
+        :return:
+        """
         return paramiko.RSAKey.from_private_key_file(key_path, password=password)
 
     @staticmethod
     def set_up_client(key, address):
+        """
+        Makes paramiko SSH client and adds the host key
+        :param key: RSA key object
+        :param address: address of server
+        :return:
+        """
         client = paramiko.SSHClient()
         client.get_host_keys().add(address, "ssh-rsa", key)
 
@@ -48,6 +65,14 @@ class SFTPClientRunner:
 
     @staticmethod
     def make_connection(client, key, address, port):
+        """
+        Connects to sftp server
+        :param client: client object to make the connection with
+        :param key: private key object
+        :param address: server address
+        :param port: server port
+        :return:
+        """
         client.connect(hostname=address, port=port, pkey=key)
 
         sftp = client.open_sftp()
@@ -57,6 +82,14 @@ class SFTPClientRunner:
 
     @staticmethod
     def move_files(file_list, source_path, target_path, sftp):
+        """
+        Moves multiple files from server to client
+        :param file_list: list of files to move
+        :param source_path: relative path to files on server
+        :param target_path: relative path to destination folder on client
+        :param sftp: sftp object
+        :return:
+        """
         for file in file_list:
             source_path_str = str(Path(source_path) / file)
             target_path_str = str(Path(target_path) / file)
