6794955caa527b08019590a2239dbc6a6d94c561
George Irving <george.irving1@gmail.com>
2023-05-25 21:11:56 +0100

    Functionality complete

--- server.py
+++ server.py
@@ -1,15 +1,18 @@
 import paramiko
 import threading
+import socket
 import os
-from paramiko import SFTPServer, SFTPAttributes, \
-    SFTPHandle, SFTP_OK
+import json
+import time
+from paramiko import SFTPServer, SFTPAttributes, SFTPHandle, SFTP_OK
+
 
 class Server(paramiko.ServerInterface):
     def __init__(self):
-        self.event = threading.Event()
+        super().__init__()
 
     def check_channel_request(self, kind, chanid):
-        if kind == 'session':
+        if kind == "session":
             return paramiko.OPEN_SUCCEEDED
 
     def check_auth_publickey(self, username, key):
@@ -17,14 +20,17 @@ class Server(paramiko.ServerInterface):
         return paramiko.AUTH_SUCCESSFUL
 
     def get_allowed_auths(self, username):
-        return 'publickey'
+        return "publickey"
 
     def check_channel_exec_request(self, channel, command):
-        # This is the command we need to parse
-        self.event.set()
         return True
 
+
 class StubSFTPHandle(SFTPHandle):
+    """
+    https://github.com/rspivak/sftpserver/blob/master/src/sftpserver/stub_sftp.py
+    """
+
     def stat(self):
         try:
             return SFTPAttributes.from_stat(os.fstat(self.readfile.fileno()))
@@ -41,7 +47,11 @@ class StubSFTPHandle(SFTPHandle):
             return SFTPServer.convert_errno(e.errno)
 
 
-class SFTPServer2(paramiko.SFTPServerInterface):
+class SFTPServer(paramiko.SFTPServerInterface):
+    """
+    https://github.com/rspivak/sftpserver/blob/master/src/sftpserver/stub_sftp.py
+    """
+
     ROOT = os.getcwd()
 
     def _realpath(self, path):
@@ -76,11 +86,10 @@ class SFTPServer2(paramiko.SFTPServerInterface):
 
     def open(self, path, flags, attr):
         path = self._realpath(path)
-        print("Testssss")
         try:
-            binary_flag = getattr(os, 'O_BINARY', 0)
+            binary_flag = getattr(os, "O_BINARY", 0)
             flags |= binary_flag
-            mode = getattr(attr, 'st_mode', None)
+            mode = getattr(attr, "st_mode", None)
             if mode is not None:
                 fd = os.open(path, flags, mode)
             else:
@@ -94,17 +103,17 @@ class SFTPServer2(paramiko.SFTPServerInterface):
             SFTPServer.set_file_attr(path, attr)
         if flags & os.O_WRONLY:
             if flags & os.O_APPEND:
-                fstr = 'ab'
+                fstr = "ab"
             else:
-                fstr = 'wb'
+                fstr = "wb"
         elif flags & os.O_RDWR:
             if flags & os.O_APPEND:
-                fstr = 'a+b'
+                fstr = "a+b"
             else:
-                fstr = 'r+b'
+                fstr = "r+b"
         else:
             # O_RDONLY (== 0)
-            fstr = 'rb'
+            fstr = "rb"
         try:
             f = os.fdopen(fd, fstr)
         except OSError as e:
@@ -160,18 +169,18 @@ class SFTPServer2(paramiko.SFTPServerInterface):
 
     def symlink(self, target_path, path):
         path = self._realpath(path)
-        if (len(target_path) > 0) and (target_path[0] == '/'):
+        if (len(target_path) > 0) and (target_path[0] == "/"):
             # absolute symlink
             target_path = os.path.join(self.ROOT, target_path[1:])
-            if target_path[:2] == '//':
+            if target_path[:2] == "//":
                 # bug in os.path.join
                 target_path = target_path[1:]
         else:
             # compute relative to path
             abspath = os.path.join(os.path.dirname(path), target_path)
-            if abspath[:len(self.ROOT)] != self.ROOT:
+            if abspath[: len(self.ROOT)] != self.ROOT:
                 # this symlink isn't going to work anyway -- just break it immediately
-                target_path = '<error>'
+                target_path = "<error>"
         try:
             os.symlink(target_path, path)
         except OSError as e:
@@ -186,10 +195,64 @@ class SFTPServer2(paramiko.SFTPServerInterface):
             return SFTPServer.convert_errno(e.errno)
         # if it's absolute, remove the root
         if os.path.isabs(symlink):
-            if symlink[:len(self.ROOT)] == self.ROOT:
-                symlink = symlink[len(self.ROOT):]
-                if (len(symlink) == 0) or (symlink[0] != '/'):
-                    symlink = '/' + symlink
+            if symlink[: len(self.ROOT)] == self.ROOT:
+                symlink = symlink[len(self.ROOT) :]
+                if (len(symlink) == 0) or (symlink[0] != "/"):
+                    symlink = "/" + symlink
             else:
-                symlink = '<error>'
+                symlink = "<error>"
         return symlink
+
+
+class ServerRunner:
+    def __init__(self):
+        # Read in control file
+        self.controls = self.get_controls(path="server_controls.json")
+
+        # Generate private key
+        self._key = paramiko.RSAKey.generate(2048)
+        self._key.write_private_key_file(
+            filename=self.controls["private_key_file_path"],
+            password=self.controls["password"],
+        )
+
+        while True:
+            # Create Paramiko transport
+            self._transport = self.make_transport(
+                address=self.controls["address"], port=self.controls["port"]
+            )
+
+            # Setup server
+            self._s = Server()
+
+            self._transport.start_server(server=self._s)
+
+            while self._transport.is_active():
+                time.sleep(1)
+
+    @staticmethod
+    def get_controls(path):
+        with open(path, "r") as f:
+            return json.load(f)
+
+    def make_transport(self, address, port):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind((address, port))
+
+        sock.listen(100)
+        client, addr = sock.accept()
+        t = paramiko.Transport(client)
+        t.add_server_key(self._key)
+
+        t.set_subsystem_handler("sftp", paramiko.SFTPServer, SFTPServer)
+        return t
+
+    def close_transport(self):
+        self._transport.close()
+
+
+if __name__ == "__main__":
+    server_runner = ServerRunner()
+
+    print("---Script terminated---")
